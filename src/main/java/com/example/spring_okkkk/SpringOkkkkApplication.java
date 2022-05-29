2a39e28c5e9dd50ac5f796c67fe2c44c31fc42c9
pascovich kaluzi <kaluzipascovich@gmail.com>
2022-05-29 13:42:06 +0200

    beginning of project

---
+++ src/main/java/com/example/spring_okkkk/SpringOkkkkApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_okkkk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOkkkkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOkkkkApplication.class, args);
+    }
+
+}
