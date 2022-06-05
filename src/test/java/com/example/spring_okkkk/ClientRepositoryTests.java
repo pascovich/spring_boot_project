a119ff2a3ad848a5bc36c5ba0473d0af2c960e35
pascovich kaluzi <kaluzipascovich@gmail.com>
2022-06-05 16:37:25 +0200

    adding template image

--- src/test/java/com/example/spring_okkkk/ClientRepositoryTests.java
+++ src/test/java/com/example/spring_okkkk/ClientRepositoryTests.java
@@ -7,7 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.stereotype.Service;
 import org.springframework.test.annotation.Rollback;
+@Service
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -28,7 +30,6 @@ public class ClientRepositoryTests {
 
         repo.save(client);
        // Client savedClient = repo.save(client);
-
 
         //Assertions.assertThat(savedClient).isnot
     }
